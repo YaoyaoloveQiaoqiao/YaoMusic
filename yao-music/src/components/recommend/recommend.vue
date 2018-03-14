814c91d3caf3cacdf15722efe6193bafe8250c92
yaoyaoloveqiaoqiao <632212519@qq.com>
2018-03-14 23:00:54 +0800

    commend page end

--- yao-music/src/components/recommend/recommend.vue
+++ yao-music/src/components/recommend/recommend.vue
@@ -1,34 +1,50 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommend.length" class="slider-wrapper">
-        <slider>
-          <div v-for="(item,item_index) in recommend" :key="item_index">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl"/>
-            </a>
-          </div>
-        </slider>
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div> 
+        <div v-if="recommend.length" class="slider-wrapper">
+          <slider>
+            <div v-for="(item,item_index) in recommend" :key="item_index">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl"/>
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="(item,index) in discList" class="item" :key="index">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl"/>
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-little">热门歌单推荐</h1>
-        <ul>
-
-        </ul>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
+  import Loading from 'base/loading/loading'
   import {getRecommend,getDiscList} from 'api/recommend'
   import {ERR_OK} from  'api/config'
 
   export default {
     data(){
       return{
-        recommend : []
+        recommend : [],
+        discList: []
       }
     },
     created(){
@@ -43,18 +59,27 @@
           }
         })
       },
-      _getDiscList(){
+    _getDiscList(){
       getDiscList().then((res) => {
         if(res.code === ERR_OK){
-          console.log(res.data.list)
+          this.discList = res.data.list
+          console.log(this.discList[0].imgurl)
         }
       })
-    }
     },
-    components: {
-      Slider
+    loadImage(){
+      if(!this.checkLoaded){
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
+  },
+  components: {
+    Slider,
+    Scroll,
+    Loading
   }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
